9088c0f0a54d1e5f10d5b07efa5af62ac84d49ed
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-11-28 22:18:13 +0100

    add solution(add, insert,get, remove methods)

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -5,7 +5,7 @@ public class LinkedList {
 
     public Link first;
     public Link last;
-    public Link length;
+    public int length;
 
 
     public void addElement(double element){
@@ -13,9 +13,82 @@ public class LinkedList {
         if (first == null){
             first = newLink;
             last = newLink;
+
         }else{
             last.nextLink = newLink;
             last = newLink;
+
         }
+        length = length+1;
+    }
+
+    public void insertElement(int index, double element){
+        indexValidation(index);
+        Link currentLink = first;
+        if (index == 0){
+            first = new Link(currentLink, element);
+        }
+        else{
+            for (int i = 0; i <=index-1 ; i++) {
+
+                if(i==index-1) {
+                    Link insertLink = new Link(currentLink.nextLink, element);
+                    currentLink.nextLink = insertLink;
+
+                }
+                currentLink =currentLink.nextLink;
+            }
+        }
+        length = length+1;
+
+    }
+    public double getElement(int index){
+        indexValidation(index);
+        Link currentLink = first;
+        for (int i = 0; i <=length-1 ; i++) {
+            if(i==index) {
+                return currentLink.element;
+            }
+            currentLink =currentLink.nextLink;
+        }
+        return first.element;
+    }
+    public void removeElement(int index){
+        indexValidation(index);
+        Link currentLink = first;
+        if (index == 0){
+            first = first.nextLink;
+        } else{
+            for (int i = 0; i <=index-1 ; i++) {
+                if(i==index-1) {
+                    Link removedLink = currentLink.nextLink;
+                    currentLink.nextLink = removedLink.nextLink;
+                    length= length-1;
+                }
+                currentLink =currentLink.nextLink;
+            }
+        }
+    }
+    public void indexValidation(int index){
+        if(index>length-1) {
+            System.out.println("Invalid index");
+            throw new IndexOutOfBoundsException("Invalid index, index greater than list length");}
+        if (index < 0){
+            System.out.println("Invalid index");
+            throw new IndexOutOfBoundsException("Invalid index, index less than list length");}
+        }
+    
+    public StringBuilder printLinkedList() {
+        Link link = first;
+        StringBuilder printLinkedList = new StringBuilder();
+        while(link.nextLink != null){
+            printLinkedList.append(link.element).append(", ");
+            link = link.nextLink;
+            if(link.nextLink==null){
+                printLinkedList.append(link.element);
+            }
+
+        }
+        return printLinkedList;
     }
 }
--- src/main/java/LinkedListDojo.java
+++ src/main/java/LinkedListDojo.java
@@ -1,11 +1,25 @@
 
 
+
 public class LinkedListDojo {
 
 
 
 
     public static void main(String[] args) {
+        LinkedList  LinkedList = new LinkedList();
+        LinkedList.addElement(2);
+        System.out.println(LinkedList.printLinkedList());
+        LinkedList.addElement(5);
+        LinkedList.addElement(7);
+        System.out.println(LinkedList.printLinkedList());
+        System.out.println(LinkedList.printLinkedList());
+        LinkedList.insertElement(2,10);
+        System.out.println(LinkedList.printLinkedList());
+        System.out.println(LinkedList.getElement(1));
+        LinkedList.removeElement(1);
+        System.out.println(LinkedList.printLinkedList());
+
 
     }
 
